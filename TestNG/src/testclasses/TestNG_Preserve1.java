8118dd9c4c374f4223ffda9ba0bfb7128410a03f
SUBBARAO BANDI <subbarao.bandi@yahoo.com>
2017-06-03 17:26:47 -0400

    added more attribute examples

---
+++ TestNG/src/testclasses/TestNG_Preserve1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package testclasses;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author subbu
+ *
+ */
+public class TestNG_Preserve1 {
+	
+	@Test
+	public void method1(){
+		System.out.println("Runs TestNG_Preserve1 -> Mehtod1..");
+	}
+	
+	@Test
+	public void method2(){
+		System.out.println("Runs TestNG_Preserve1 -> Mehtod2..");
+	}
+
+}
---
+++ TestNG/src/testclasses/TestNG_Preserve2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package testclasses;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author subbu
+ *
+ */
+public class TestNG_Preserve2 {
+	@Test
+	public void method1(){
+		System.out.println("Runs TestNG_Preserve2 -> Mehtod1..");
+	}
+	
+	@Test
+	public void method2(){
+		System.out.println("Runs TestNG_Preserve2 -> Mehtod2..");
+	}
+
+
+}
